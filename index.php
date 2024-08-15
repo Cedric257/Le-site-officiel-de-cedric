8baae1cb85eee089f9e03de2038bcb1ed36a5219
Cedric257 <cedricakimana257@gmail.com>
2024-08-15 02:30:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+
+<?php @include 'header.php' ; ?>
+    <section class="homepage" id="home">
+      <div class="content">
+        <div class="text">
+          <h3>Bienvenue dans le Site Professionnel de Cedric Developpeur</h3>
+          <p>
+            Vous voulez des SiteWeb Professionnel? Cedric est ici pour vous. 
+            <br> Nous faisons beaucoup de choses commes le Design,
+             <br>Faire des Invitations, <br>Affiche publicitaire et Concert,<br> Creation de Logo , <br> Formations sur le Bureautique et l'Adobe Photoshop.
+          <br>
+        <br>
+      Cedric est aussi un Informaticien , Technicien en Informatique</p>
+        </div>
+        <a href="#services">Nos Services</a>
+      </div>
+    </section>
+
+    <section class="services" id="services">
+      <h2>Nos Services</h2>
+      <p>Voila la liste de nos services.</p>
+      <ul class="cards">
+        <li class="card">
+          <img src="images/tents.jpg" alt="img">
+          <h3>Programmation</h3>
+          <p>Elaboration et Codification des SiteWeb.</p>
+        </li>
+        <li class="card">
+          <img src="images/bags.jpg" alt="img">
+          <h3>Design</h3>
+          <p>Design des Invitations, Logo , etc. Avec Adobe Photoshop, Illustrator et Lightroom</p>
+        </li>
+        <li class="card">
+          <img src="images/stoves.jpg" alt="img">
+          <h3>Formation</h3>
+          <p>Nous avons offert la formation qui permet a ceux qui la preparent d'acquerir des competences professionnelles variees et a se servir des diferents logiciels sur un ordinateur.</p>
+        </li>
+        <li class="card">
+          <img src="images/backpacks.jpg" alt="img">
+          <h3>Maintenance des equipements Informatique</h3>
+          <p>Nous consistons a assure le bon foctionnements du materiel, des logiciels et des reseaux informatiques.</p>
+        </li>
+        <li class="card">
+          <img src="images/chair.jpg" alt="img">
+          <h3>Reseaux informatiques</h3>
+          <p>Rendre les ceremonies heureuses.</p>
+        </li>
+        <li class="card">
+          <img src="images/light.jpg" alt="img">
+          <h3>Electricien</h3>
+          <p>Illuminate your campsite with our reliable and energy-efficient camping lights.</p>
+        </li>
+      </ul>
+    </section>
+
+    <section class="portfolio" id="portfolio">
+      <h2>Administration</h2>
+      <p>L'administration de notre equipe.</p>
+      <ul class="cards">
+        <li class="card">
+          <img src="images/camping-1.jpg" alt="img">
+          <h3>AKIMANA Cedric</h3>
+          <p>C.E.O</p>
+        </li>
+        <li class="card">
+          <img src="images/camping-2.jpg" alt="img">
+          <h3>NIZIGIYIMANA Daniel</h3>
+          <p>Vice-C.E.O.</p>
+        </li>
+        <li class="card">
+          <img src="images/camping-3.jpg" alt="img">
+          <h3>KWIZERA Rosine</h3>
+          <p>Secretaire.</p>
+        </li>
+        <li class="card">
+          <img src="images/camping-4.jpg" alt="img">
+          <h3>BIGIRIMANA Serges</h3>
+          <p>Conseille.</p>
+        </li>
+        <li class="card">
+          <img src="images/camping-5.jpg" alt="img">
+          <h3>BARUTWANAYO Gedeon</h3>
+          <p>Tresolie.</p>
+        </li>
+        <li class="card">
+          <img src="images/camping-6.jpg" alt="img">
+          <h3>NININAHAZWE Elyse</h3>
+          <p>Deejay.</p>
+        </li>
+      </ul>
+    </section>
+
+    <section class="about" id="about">
+      <h2>A Propos de Nous</h2>
+      <p>Developpeurs web.</p>
+      <div class="row company-info">
+        <h3>Notre Stories</h3>
+        <p>c'est tres compliques.</p>
+      </div>
+      <div class="row mission-vision">
+        <h3>Notre Mission</h3>
+        <p>De devellope la technologie dans notre pays .</p>
+        <h3>Notre Vision</h3>
+        <p>Une bonne connaissance.</p>
+      </div>
+      <div class="row team">
+        <h3>Notre equipe</h3>
+        <ul>
+          <li>AKIMANA Cedric - Developpeur,Technicien, Designeur et C.E.O</li>
+          <li>Daniel Amisi - Informaticien, Technicien hardware</li>
+          <li>BARUTWANAYO Gedeon - Informaticien , Technicien </li>
+          <li>NGENDAHAYO Teddy - Informaticien, Technicien software</li>
+          <li>NININAHAZWE Elyse - Informaticien, Deejay (DJ)</li>
+          <li>IZABAYO Clovis - Informaticien, Journaliste</li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="contact" id="contact">
+      <h2>Contactez-Nous</h2>
+      <p>Contactez-Nous pour toute demande de renseignements ou commentaires.</p>
+      <div class="row">
+        <div class="col information">
+          <div class="contact-details">
+            <p><i class="fas fa-map-marker-alt"></i> +257 Bujumbura, Kinama, Q.Kanga </p>
+            <p><i class="fas fa-envelope"></i> cedicakimana257@gmail.com</p>
+            <p><i class="fas fa-phone"></i> (+257) 61 377 295</p>
+            <p><i class="fas fa-clock"></i> Lundi - Vendredi: 8:00 AM - 17:00 PM</p>
+            <p><i class="fas fa-clock"></i> Samedi: 10:00 AM - 15:00 PM</p>
+            <p><i class="fas fa-clock"></i> Dimanche: Ferme</p>
+            <p><i class="fas fa-globe"></i> www.cedricdev.com</p>
+          </div>          
+        </div>
+        <div class="col form">
+          <form>
+            <input type="text" placeholder="Nom*" required>
+            <input type="email" placeholder="Email*" required>
+            <textarea placeholder="Message*" required></textarea>
+            <button id="submit" type="submit">Envoyez un message</button>
+          </form>
+        </div>
+      </div>
+    </section>
+
+  
+    <section class="follow" id="follow">
+      <h2>Suivez-Nous</h2>
+    <div class="color"></div>
+        <div class="color"></div>
+        <div class="color"></div>
+        <ul>
+            <li><a href="#"><i class="fa-brands fa-youtube"></i> </a></li>
+            <li><a href="#"><i class="fa-brands fa-facebook"></i> </a></li>
+            <li><a href="#"><i class="fa-brands fa-instagram"></i> </a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i> </a></li>
+            <li><a href="#"><i class="fa-brands fa-github"></i> </a></li>
+            <li><a href="#"><i class="fa-brands fa-whatsapp"></i> </a></li>
+            </section>
+
+  <footer>
+      <div>
+        <span>Copyright © Cedric2023. Touts droits reserves</span>
+        <span class="link">
+            <a href="#">Acceuil</a>
+           
+        </span>
+      </div>
+    </footer>
+
+  </body>
+</html>
